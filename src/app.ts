5c71059a9a82aa4ded070745b2d6215e428c2f82
sadmaningithub <sadmanahmed261@gmail.com>
2025-12-01 05:26:41 +0600

    mongodb connection successful

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,8 @@ import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
 
+app.get('/', (req: Request, res: Response)=>{
+    res.send('Welcome to library management api.')
+})
+
 export default app;
