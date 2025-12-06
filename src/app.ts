f3cd959e0130762ea3be1941cfa89a545aeb90f7
sadmaningithub <sadmanahmed261@gmail.com>
2025-12-06 13:20:24 +0600

    create and validate book

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,48 @@
 import express, { Application, Request, Response } from 'express';
+import { model, Schema } from 'mongoose';
 
 const app: Application = express();
 
-app.get('/', (req: Request, res: Response)=>{
+app.use(express.json());
+
+const bookSchema = new Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    genre: {
+        type: String,
+        required: true,
+        enum: ['FICTION', 'NON_FICTION', 'SCIENCE', 'HISTORY', 'BIOGRAPHY', 'FANTASY']
+    },
+    isbn: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: String,
+    copies: {
+        type: Number,
+        required: true,
+        min: [0, 'Number of copies available cannot be negative.']
+    },
+    available: {
+        type: Boolean,
+        default: true
+    }
+});
+
+const Book = model('Book', bookSchema)
+
+app.post('/api/books', async(req: Request, res: Response) => {
+    const body = req.body;
+    const book = await Book.create(body);
+    res.status(201).json({
+            success: true,
+            message: "Book created successfully",
+            book
+    })
+})
+
+app.get('/', (req: Request, res: Response) => {
     res.send('Welcome to library management api.')
 })
 
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
+import { Server } from 'http';
 import mongoose from "mongoose";
 import app from "./app";
 
-let server;
+let server: Server;
 
 const PORT = 5000;
 
 async function main(){
     try {
-        await mongoose.connect('mongodb+srv://libMan:PyZYg5eHIEblC2ZQ@cluster0.2vtceto.mongodb.net/?appName=Cluster0');
+        await mongoose.connect('mongodb+srv://libMan:PyZYg5eHIEblC2ZQ@cluster0.2vtceto.mongodb.net/libraryDB?appName=Cluster0');
         console.log('Successfully connected to mongodb!');
         server = app.listen(PORT, () => {
             console.log(`App is listening on port ${PORT}`);
